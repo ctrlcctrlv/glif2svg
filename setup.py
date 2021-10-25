1ce4260fc75cce0d11280d05c54a8ab458c4ac46
Fredrick Brennan <copypaste@kittens.ph>
2021-10-25 04:05:24 -0400

    Version 1.2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ config = {
     'description': 'Convert UFO .glif files to SVG',
     'long_description': open('README.md', 'r').read(),
     'license': 'MIT',
-    'version': '1.1.0',
+    'version': '1.2.0',
     'install_requires': install_requires,
     'classifiers': [
         "Programming Language :: Python",
